c48d4c7d2587f66317f446b3a0c6f707675e6e0f
Ben Scobie <benscobie@users.noreply.github.com>
2022-10-19 18:02:48 +0100

    Fix bool args

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import os
 import qbittorrentapi
 import time
 import argparse
@@ -17,7 +18,7 @@ parser.add_argument(
     "-p", "--password", action=EnvDefault, envvar='QBIT_PASSWORD',
     help="Specify the qBittorrent password (can also be specified using QBIT_PASSWORD environment variable)")
 parser.add_argument(
-    "-d", "--dryrun", action=EnvDefault, required=False, default=False, envvar='QBIT_DRY_RUN',
+    "-d", "--dryrun", action='store_true', default=None,
     help="Specify whether to actually delete torrents or not (can also be specified using QBIT_DRY_RUN environment variable)")
 parser.add_argument(
     "-dl", "--disklimit", action=EnvDefault, type=int, required=False, default=0, envvar='QBIT_DISK_LIMIT_BYTES',
@@ -26,7 +27,7 @@ parser.add_argument(
     "-s", "--sleep", action=EnvDefault, type=int, default=30, envvar='QBIT_SLEEP_DURATION',
     help="Specify how long to sleep between runs, only relevant if --runonce is FALSE (can also be specified using QBIT_SLEEP_DURATION environment variable)")
 parser.add_argument(
-    "-ro", "--runonce", action=EnvDefault, type=bool, required=False, default=False, envvar='QBIT_RUN_ONCE',
+    "-ro", "--runonce", action='store_true', default=None,
     help="Specify whether to run once or forever (can also be specified using QBIT_RUN_ONCE environment variable)")
 parser.add_argument(
     "-seed", "--seedduration", action=EnvDefault, type=int, required=False, default=0, envvar='QBIT_SEED_DURATION',
@@ -43,9 +44,27 @@ parser.add_argument(
     envvar='QBIT_COLLECTION_SEED_DURATION',
     help="Specify how long a multi file torrent should be seeded for in seconds before deletion (can also be specified using QBIT_COLLECTION_SEED_DURATION environment variable)")
 parser.add_argument(
-    "-df", "--deletefiles", action=EnvDefault, type=bool, required=True, default=False, envvar='QBIT_DELETE_FILES',
+    "-df", "--deletefiles", action='store_true', default=None,
     help="Specify whether to delete files or just the torrent on deletion (can also be specified using QBIT_DELETE_FILES environment variable)")
 args = parser.parse_args()
+
+if args.runonce is None:
+    if 'QBIT_RUN_ONCE' in os.environ:
+        args.runonce = True
+    else:
+        args.runonce = False
+
+if args.dryrun is None:
+    if 'QBIT_DRY_RUN' in os.environ:
+        args.dryrun = True
+    else:
+        args.dryrun = False
+
+if args.deletefiles is None:
+    if 'QBIT_DELETE_FILES' in os.environ:
+        args.deletefiles = True
+    else:
+        args.deletefiles = False
 
 qbt_client = qbittorrentapi.Client(
     host=args.host,
